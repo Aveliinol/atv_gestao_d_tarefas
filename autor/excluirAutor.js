36bdd0971ddba080e3d815644fbd9797289fd0c1
xexeu.l <lugabriell084@outlook.com.br>
2024-10-17 17:27:16 -0300

    Edit: alterei umas const no excluirAutor.js

--- autor/excluirAutor.js
+++ autor/excluirAutor.js
@@ -2,6 +2,13 @@ const {autores} = require("./autores");
 
 function excluirAutor(nome) {
  autores.forEach(autor =>(
-    ii
+    l
  ))
 }
+
+const dados = [1,2,3,4,5,6,0];
+
+const index = dados.indexOf(4);
+
+const element = dados.splice(index, 1); 
+
